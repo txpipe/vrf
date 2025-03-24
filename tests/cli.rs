be9d3bd2449480c5495639213ae3c9aff4c410f3
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-03-24 17:37:20 +0100

    CLI test for version

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,10 +2,20 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
-fn usage_when_help_arg() {
+fn correct_output_help_arg() {
     let mut cmd = Command::cargo_bin("vrf_dalek").unwrap();
     cmd.arg("--help")
         .assert()
         .success()
         .stdout(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn correct_output_version_arg() {
+    let mut cmd = Command::cargo_bin("vrf_dalek").unwrap();
+    let ver = "vrf_dalek 0.1.0";
+    cmd.arg("--version")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(ver));
 }
